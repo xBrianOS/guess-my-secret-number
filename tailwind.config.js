0c6e7734063c49dba97e0ecf413d9bb857c0a0c0
XBrianOS <brian.os2003@gmail.com>
2024-08-11 11:17:42 -0400

    upload: tailwindcss dependencies, gitignore & md file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#100C08",
+        win: "#03C03C",
+        lose: "#8B0000",
+      },
       fontFamily: {
-        arcade: ["arcade", "font-mono"],
+        arcade: ["arcade"],
       },
     },
   },
